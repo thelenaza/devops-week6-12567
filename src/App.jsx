c3fbb8ccbf8f820ea35a0bef2b2137a611db4649
Your Namethelenaza <kaxtie07@gmail.com>
2024-06-25 14:11:50 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1 style={{color: "purple" , textAlign: "center"}}>Welcome to MyHome</h1>
-      <hr/>
-      <h3 style={{textAlign: "center"}}>My name is Leena</h3>
-      <img src="https://cdnb.artstation.com/p/assets/images/images/049/246/167/large/alice-le-coguic-teapot-shot01.jpg?1652053257"  
-      width={350}
-      height={250}
-      alt="" />
-    </div>
-  )
-}
+    <>
+      <h1 style={{ color: "purple", textAlign: "center" }}>
+        Welcome to MyHome
+      </h1>
+      <hr />
+      <div style={{ textAlign: "center" }}>
+        <h3>My name is Leena</h3>
+        <img
+          src="https://cdnb.artstation.com/p/assets/images/images/049/246/167/large/alice-le-coguic-teapot-shot01.jpg?1652053257"
+          width={350}
+          height={250}
+          alt=""
+        />
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
