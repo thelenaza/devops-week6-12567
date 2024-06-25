5e32aee3dfe7ae4cb0ba3ff04b81c12664b01349
thelenaza <kaxtie07@gmail.com>
2024-06-25 13:42:55 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{color: "purple" , textAlign: "center"}}>Welcome to MyHome</h1>
+      <hr/>
+      <h3 style={{textAlign: "center"}}>My name is Leena</h3>
+      <img src="https://cdnb.artstation.com/p/assets/images/images/049/246/167/large/alice-le-coguic-teapot-shot01.jpg?1652053257"  
+      width={350}
+      height={250}
+      alt="" />
+    </div>
+  )
+}
+
+export default App
